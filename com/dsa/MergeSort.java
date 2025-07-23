2e38d3608b7513c40f427219fa2854adc2a6b031
Sunny1293 <71592871+Sunny1293@users.noreply.github.com>
2025-07-23 14:18:24 +0000

    add java code

---
+++ com/dsa/MergeSort.java
@@ -0,0 +1,60 @@
+package com.dsa;
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] arr = {38, 27, 43, 3, 9, 82, 10};
+        System.out.println("Original array:" + Arrays.toString(arr));
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.println("Sorted array:" + Arrays.toString(arr));
+    }
+
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+
+            // Sort first and second halves
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            // Merge the sorted halves
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+      
+     int[] temp = new int[right - left + 1];
+        int i = left; // Starting index for left subarray
+        int j = mid + 1; // Starting index for right subarray
+        int k = 0; // Starting index for merged subarray
+
+        // Merge the two halves into temp[]
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        // Copy remaining elements of left subarray, if any
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        // Copy remaining elements of right subarray, if any
+        while (j <= right) {
+            temp[k++] = arr[j++];
+        }
+
+        // Copy the merged elements back to the original array
+        for (i = left, k = 0; i <= right; i++, k++) {
+            arr[i] = temp[k];
+        }
+        
+    }
+
+}
