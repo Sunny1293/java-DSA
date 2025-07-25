62e9f09893c407c824e255c605b917ded05a2e12
Sunny1293 <71592871+Sunny1293@users.noreply.github.com>
2025-07-25 10:32:46 +0000

    java code for insertion sort

---
+++ com/sorting/InsertionSort.java
@@ -0,0 +1,28 @@
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        int[] arr = { 64, 25, 12, 22, 11 };
+        System.out.println("Original array: " + java.util.Arrays.toString(arr));
+        insertionSort(arr);
+        System.out.println("Sorted array: " + java.util.Arrays.toString(arr));
+    }
+
+    private static void insertionSort(int[] arr) {
+        int n = arr.length;
+        // Traverse through 1 to n
+        for (int i = 1; i < n; i++) {
+            // The element to be inserted
+            int key = arr[i];
+            // Find the position where key should be inserted
+            int j = i - 1;
+            // Move elements of arr[0..i-1], that are greater than key,
+            // to one position ahead of their current position
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j]; // Shift element
+                j--;
+            }
+            arr[j + 1] = key; // Insert key
+        }
+
+    }
+}
